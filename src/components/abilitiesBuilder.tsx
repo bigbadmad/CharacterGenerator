b038d48818992816eabb324cf5315c8dccb4e0ba
Matthew Allen <github@madtv.me.uk>
2023-01-23 23:37:45 +0000

    cont.

---
+++ src/components/abilitiesBuilder.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+import { IDropdownProps } from '../interfaces/interfaces';
+import Dropdown from './dropdown';
+
+const AbilityBuilder: FC<void> = () => {
+
+    return (
+        <div>
+            <p>place holder</p>
+        </div>
+    );
+};
+
+export default AbilityBuilder;
